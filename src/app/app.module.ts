87896794cff0761f98eba2817e0d30a9f142c880
Prince-Kr-Sharma <prince30sharma52@gmail.com>
2021-08-16 22:29:43 +0530

    Git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EligibilityComponent } from './eligibility/eligibility.component';
+import { IdentitydetailsComponent } from './identitydetails/identitydetails.component';
+import { PersonaldetailsComponent } from './personaldetails/personaldetails.component';
+import { LoanofferComponent } from './loanoffer/loanoffer.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { AdmindashboardComponent } from './admindashboard/admindashboard.component';
+import { AdminmainComponent } from './adminmain/adminmain.component';
+import { CustomersDetailsService } from './customers-details.service';
+import { LogoutComponent } from './logout/logout.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EligibilityComponent,
+    IdentitydetailsComponent,
+    PersonaldetailsComponent,
+    LoanofferComponent,
+    LoginComponent,
+    SignupComponent,
+    AdminloginComponent,
+    AdmindashboardComponent,
+    AdminmainComponent,
+    LogoutComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [CustomersDetailsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
